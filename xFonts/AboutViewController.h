0e10e146cabbae0bcbefee9cce5bafcbbdc211c0
Craig Hockenberry <chockenberry@users.noreply.github.com>
2020-04-19 13:17:53 -0700

    About view improvements.

---
+++ xFonts/AboutViewController.h
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.h
+//  xFonts
+//
+//  Created by Craig Hockenberry on 4/19/20.
+//  Copyright © 2020 manolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AboutViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ xFonts/AboutViewController.m
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.m
+//  xFonts
+//
+//  Created by Craig Hockenberry on 4/19/20.
+//  Copyright © 2020 manolo. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *versionLabel;
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	NSString *productVersion = [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+	NSString *productBuild = [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleVersion"];
+	self.versionLabel.text = [NSString stringWithFormat:@"VERSION %@ (%@)", productVersion, productBuild];
+}
+
+@end
